ae18aeb99886a839f8080c8a747b2472a86449a8
James Anderson <james.anderson@wright.edu>
2026-09-24 12:04:08 -0400

    added and commented the linked list `Node` class we will continue to use

--- in_class_demos/linked_list/main.cpp
+++ in_class_demos/linked_list/main.cpp
@@ -3,6 +3,21 @@
 
 #include <iostream>
 
+// A Node is a segment of a linked list In this case, the list will store `int`
+// and `next` will point to the next node in the list.
+// For the last node in the list, `next == nullptr` to signal there are no more
+// nodes/values in the list
+class Node {
+public:
+    int value;
+    Node* next;
+};
+
 int main() {
     std::cout << "Linked List Demo" << std::endl;
+
+    // head will point to the first node in our list, and each node will
+    // point to the next node. If the list is empty, `head == nullptr`. If
+    // there is one thing in the list, `head->next == nullptr`.
+    Node* head = nullptr;
 }
